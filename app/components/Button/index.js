f6ecee2ed35621609ad120c2d8f8e0421a0d4427
Nick Krambousanos <nkrambo@gmail.com>
2017-02-15 00:37:04 -0800

    Update button component

--- app/components/Button/index.js
+++ app/components/Button/index.js
@@ -1,34 +1,44 @@
 
-import styled from 'styled-components';
+/**
+* A common button, if you pass it a prop "route" it'll render a link to a react-router route
+* otherwise it'll render a link with an onclick
+*/
 
-const Button = styled.button`
-  background: ${(props) => props.primary ? '#00B5AD' : '#E0E1E2'};
-  color: ${(props) => props.primary ? 'white' : 'rgba(0, 0, 0, 0.6)'};
-  cursor: pointer;
-  display: inline-block;
-  min-height: 1em;
-  outline: 0;
-  border: none;
-  vertical-align: baseline;
-  margin: 0 0.25em 0 0;
-  padding: 0.75em 1.5em;
-  text-transform: none;
-  text-shadow: none;
-  font-weight: 700;
-  line-height: 1em;
-  font-style: normal;
-  text-align: center;
-  text-decoration: none;
-  border-radius: 2px;
-  user-select: none;
-  transition: opacity 0.1s ease, background-color 0.1s ease, color 0.1s ease, box-shadow 0.1s ease, background 0.1s ease;
+import React, { PropTypes, Children } from 'react';
 
-  &:hover {
-    background: ${(props) => props.primary ? '#009C95' : '#CACBCD'};
-    color: ${(props) => props.primary ? 'white' : 'rgba(0, 0, 0, 0.8)'};
-    background-image: none;
-    box-shadow: 0 0 0 1px transparent inset, 0 0 0 0 rgba(34, 36, 38, 0.15) inset;
+import A from './A';
+import StyledButton from './StyledButton';
+import Wrapper from './Wrapper';
+
+function Button({ href, onClick, children, handleRoute }) {
+  // Render an anchor tag
+  let button = (
+    <A href={href} onClick={onClick}>
+      {Children.toArray(children)}
+    </A>
+  );
+
+  // If the Button has a handleRoute prop, we want to render a button
+  if (handleRoute) {
+    button = (
+      <StyledButton onClick={handleRoute}>
+        {Children.toArray(children)}
+      </StyledButton>
+    );
   }
-`;
+
+  return (
+    <Wrapper>
+      {button}
+    </Wrapper>
+  );
+}
+
+Button.propTypes = {
+  handleRoute: PropTypes.func,  // eslint-disable-line react/require-default-props
+  href: PropTypes.string,       // eslint-disable-line react/require-default-props
+  onClick: PropTypes.func,      // eslint-disable-line react/require-default-props
+  children: PropTypes.node.isRequired,
+};
 
 export default Button;
